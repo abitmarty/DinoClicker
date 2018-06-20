2f488ebeb2349707e2a343cf30b175b14c5739c6
abitmarty <martijn.totte@gmail.com>
2018-06-20 12:11:46 +0200

    Upload 1

--- app/src/main/java/com/example/martijn/toolbar/bank.java
+++ app/src/main/java/com/example/martijn/toolbar/bank.java
@@ -69,7 +69,7 @@ public class bank extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Account");
 
-        //Top scores er in zetten
+        //Top scores er in zettenn
         fillListToppers();
 
 
